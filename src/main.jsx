87995562b49fa733c803c60106557702c7897a1b
Daniel Marquez <dmarquez9@gatech.edu>
2026-08-26 20:13:14 -0400

    Disabled zoom behavior messing up the UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import DisableZoom from './DisableZoom.jsx';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <>
+    <DisableZoom />
+    <App />
+  </>
+);
