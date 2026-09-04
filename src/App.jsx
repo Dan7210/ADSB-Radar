ba61e3726b4cb3c70d6e0f2085555e5311391503
Daniel Marquez <danielrmarquez22@gmail.com>
2026-09-04 10:41:48 -0400

    Using hash routing to make routing github pages compatible

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Routes,
     Route,
 } from "react-router-dom";
 import MapApp from "./MapApp";
 import YJFCMap from "./YJFCMap";
 
-
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/ADSB-Radar" element={<MapApp />} />
-                <Route path="/ADSB-Radar/YJFC" element={<YJFCMap />} />
+                <Route path="/" element={<MapApp />} />
+                <Route path="/YJFC" element={<YJFCMap />} />
             </Routes>
         </Router>
     );
 }
 
-export default App
+export default App;
